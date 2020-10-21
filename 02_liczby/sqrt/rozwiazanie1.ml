ddf9c7c81321aeb4fd2c5b63fec0127735bec0da
jacekchrzaszcz <chrzaszcz@mimuw.edu.pl>
2020-10-21 11:43:31 +0200

    Update rozwiazanie1.ml
    Correctness arguments added in the comments

--- 02_liczby/sqrt/rozwiazanie1.ml
+++ 02_liczby/sqrt/rozwiazanie1.ml
@@ -1,7 +1,12 @@
+(* x >= 0  ==>  result = n such that n>=0 and n^2 <= x < (n+1)^2 *)
 let sqrt x = 
+    (* s <= x, s=k^2, k>=0  ==>  result = n as above *)   
     let rec aux s k = 
         let nw = 2 * (k + 1) - 1 in
-        if s + nw <= x then 
+        if s + nw <= x then  (* (k+1)^2 <= x *) 
             aux (s + nw) (k + 1)
-        else k in
+            (* new s=(k+1)^2, s <= x, k+1 >=0 OK for k+1  *)
+        else k (* k^2 <= x < (k+1)^2  OK *) 
+    in
     aux 0 0
+        (* s <= x, s=k^2, k>=0 OK for k=0 *)
